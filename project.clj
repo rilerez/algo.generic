d30f7f3169ff9ad7b7aea316953a246c3604e5d8
Riley Levy <rileylevy95@gmail.com>
2020-11-28 17:30:56 -0500

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject algo.generic "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :min-lein-version "2.7.1"
+
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.773"]]
+
+  :source-paths ["src"])
